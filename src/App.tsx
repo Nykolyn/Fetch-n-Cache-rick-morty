4b8c2cf64ceb6dccd384286afc6bb78bce4e2948
Nykolyn <nykolynandrii@gmail.com>
2021-09-05 14:09:39 +0300

    feat: add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import { Box } from '@material-ui/core';
+import { useState } from 'react';
+
+import Search from './Components/Search';
 
 const App = () => {
-  return <Box display="flex" p={8} justifyContent="space-between"></Box>;
+  const [isFetching, setIsFetching] = useState(false);
+
+  const onSearch = (search: string) => {
+    setIsFetching(true);
+    setIsFetching(false);
+  };
+
+  return (
+    <Box display="flex" p={12} justifyContent="space-between">
+      <Box>
+        <Search isFetching={isFetching} onSearch={onSearch} />
+      </Box>
+    </Box>
+  );
 };
 
 export default App;
---
+++ src/Components/Search.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Button, InputAdornment, makeStyles, TextField } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  // hide default browser behaviour for number input
+  input: {
+    '& input[type=number]': {
+      '-moz-appearance': 'textfield',
+    },
+    '& input[type=number]::-webkit-outer-spin-button': {
+      '-webkit-appearance': 'none',
+      margin: 0,
+    },
+    '& input[type=number]::-webkit-inner-spin-button': {
+      '-webkit-appearance': 'none',
+      margin: 0,
+    },
+  },
+}));
+
+type TSearchProps = {
+  onSearch: (search: string) => void;
+  isFetching: boolean;
+};
+
+const Search: React.FC<TSearchProps> = ({ onSearch, isFetching }) => {
+  const classes = useStyles();
+
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(input);
+  };
+
+  return (
+    <TextField
+      value={input}
+      className={classes.input}
+      type="number"
+      onChange={handleInputChange}
+      label="Enter any number"
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <Button color="primary" disabled={isFetching} size="small" onClick={handleSearch}>
+              Search
+            </Button>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export default Search;
